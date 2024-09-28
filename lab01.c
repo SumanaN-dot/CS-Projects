5df700d71b636aeef91eeff01f3972715eefff5f
SumanaN-dot <sumanareddy0812@gmail.com>
2024-09-28 10:25:43 -0400

    Add files via upload

---
+++ lab01.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(){
+  printf("Hello World!\n");
+  printf("I setup my odin account to use gcc and g++ version 11.2.0.\n");
+  printf("I practiced using basic Unix commands.\n");
+  return 0;
+}
